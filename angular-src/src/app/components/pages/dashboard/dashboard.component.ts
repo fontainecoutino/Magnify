24793e506b42d8a6da38038daa71060452734123
fontainecoutino <fontaine@coutino.org>
2023-03-02 18:20:17 -0700

    dashboard can now login and maintain token for api calls

--- angular-src/src/app/components/pages/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { tap } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from 'src/environment/environment';
 
 @Component({
@@ -9,56 +9,47 @@ import { environment } from 'src/environment/environment';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  stateKey = 'spotify_auth_state';
+  access_token: string | null;
 
-  params: any;
-  access_token: string | null;;
-  state: string | null;;
-  storedState: string | null;
-
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute ) {
     this.access_token = "";
-    this.state = "";
-    this.storedState = "";
   }
 
   ngOnInit() {
-    this.params = this.getHashParams();
-
-    this.access_token = this.params.access_token,
-    this.state = this.params.state,
-    this.storedState = localStorage.getItem(this.stateKey);
-
-    if (this.access_token && (this.state == null || this.state !== this.storedState)) {
-      alert('There was an error during the authentication');
-    } else {
-      localStorage.removeItem(this.stateKey);
+    if (this.getHashParams()){   // log in to spotify was succesful
       if (this.access_token) {
-        var url = 'https://api.spotify.com/v1/me';
-
-        this.http.get(url, {headers: new HttpHeaders({'Authorization': 'Bearer ' + this.access_token})})
-        .pipe(
-          tap( // Log the result or error
-          {
-            next: (data) => console.log(data),
-            error: (error) => console.log(error)
-          }
-          )
-        );
-        
+        this.getProfileInfo()
       } else {
-          console.log('sucess!')
+        this.errorLoginIn()
       }
+
+    } else { // not logged in so login to spotify
+      this.spotifyClientLogIn()
     }
   }
+  
+  errorLoginIn() {
+    console.log('There was an error during the authentication');
+    this.router.navigate([ '/' ])
+  }
 
-  onLogin() {
+  getProfileInfo() {
+    var url = 'https://api.spotify.com/v1/me';
+    console.log("here", url, this.access_token)
+
+    var req = this.http.get(url, {headers: new HttpHeaders({'Authorization': 'Bearer ' + this.access_token})})
+    
+    req.subscribe(
+      {
+        next: (v) => console.log(v),
+        error: (e) => console.error(e)
+    })
+  }
+
+  private spotifyClientLogIn() {
     var client_id = environment.spotifyClientID;
     var redirect_uri = environment.spotifyRedirctURI;
 
-    var state = this.generateRandomString(16);
-
-    localStorage.setItem(this.stateKey, state);
     var scope = 'user-read-private user-read-email';
 
     var url = 'https://accounts.spotify.com/authorize';
@@ -66,46 +57,33 @@ export class DashboardComponent {
     url += '&client_id=' + encodeURIComponent(client_id);
     url += '&scope=' + encodeURIComponent(scope);
     url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
-    url += '&state=' + encodeURIComponent(state);
 
     window.location.href = url;
   }
-    
 
   /**
    * Obtains parameters from the hash of the URL
    * @return Object
    */
   private getHashParams() {
-    // var hashParams = {};
-    // var e, r = /([^&;=]+)=?([^&;]*)/g,
-    //     q = window.location.hash.substring(1);
-    // while ( e = r.exec(q)) {
-    //     hashParams[e[1]] = decodeURIComponent(e[2]);
-    // }
-    // return hashParams;
+    var parameters = this.router.parseUrl(this.router.url).fragment;
 
-    // var hash = window.location.hash.substr(1);
+    if (parameters){
+      var parametersList = parameters.split("&")
 
-    // var result = hash.split('&').reduce(function (res, item) {
-    //     var parts = item.split('=');
-    //     res[parts[0]] = parts[1];
-    //     return res;
-    // }, {});
-  }
-
-  /**
-   * Generates a random string containing numbers and letters
-   * @param  {number} length The length of the string
-   * @return {string} The generated string
-   */
-  private generateRandomString(length: number) {
-    var text = '';
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    for (var i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
+      for (var parameterPair of parametersList){
+        var parameterPairSplit = parameterPair.split('=');
+        if (parameterPairSplit.length != 2) continue;
+        
+        var parameterName = parameterPairSplit[0]
+        var parameterValue = parameterPairSplit[1]
+        if (parameterName == "access_token"){
+          this.access_token = parameterValue;
+        }
+      }
+      return true
+    } else {
+      return false
     }
-    return text;
-  };
+  }
 }
