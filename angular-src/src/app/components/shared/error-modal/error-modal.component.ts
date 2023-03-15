8399455508ea07aa7eeb1d6d38b0ad27482c2ba0
fontainecoutino <fontaine@coutino.org>
2023-03-14 17:18:44 -0700

    error modal now redirects to homepage after 5s

--- angular-src/src/app/components/shared/error-modal/error-modal.component.ts
+++ angular-src/src/app/components/shared/error-modal/error-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error-modal',
@@ -7,4 +8,17 @@ import { Component, Input } from '@angular/core';
 })
 export class ErrorModalComponent {
   @Input() error: string = "";
+
+  constructor( private router: Router ){}
+
+  async ngOnInit() {
+    await this.sleep(5);
+    this.router.navigate([ '/' ]);
+  }
+
+  async sleep(seconds: number){
+    await new Promise(resolve => {
+      return setTimeout(resolve, seconds * 1000);
+  });
+  }
 }
